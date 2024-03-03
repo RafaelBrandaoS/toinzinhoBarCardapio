cb04ec35a3c928f932308f7791f5c77ded6711dd
Rafael Brandão da Silva <113553591+RafaelBrandaoS@users.noreply.github.com>
2024-03-03 19:17:06 -0300

    animações de scroll

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
 })
+
+var item = document.querySelectorAll("[data-anima]")
+
+function animeScroll() {
+    let windowTop = window.pageYOffset + window.innerHeight * 1
+
+    item.forEach((element) => {
+        if(windowTop > element.offsetTop) {
+            element.classList.add("animate")
+        } else {
+            element.classList.remove("animate")
+        }
+    })
+}
+
+animeScroll()
+
+window.addEventListener("scroll", function() {
+    animeScroll()
+})
