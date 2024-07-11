11936d11a4687b694bca160c593216e7660d26cd
Rafael Brandão da Silva <113553591+RafaelBrandaoS@users.noreply.github.com>
2024-07-11 09:40:27 -0300

    Atualizei o pip e preparei o projeto para a ospedagem

--- python/conexao.py
+++ python/conexao.py
@@ -1,12 +1,17 @@
 import mysql.connector
-host = 'roundhouse.proxy.rlwy.net'
-usuario = 'root'
-senha = 'LVPufVkmQJJGJnxGSPJHgkWaeeMhVqPp'
-porta = '43227'
-banco = 'cardapio'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv('DB_HOST')
+DB_USUARIO = os.getenv('DB_USUARIO')
+DB_SENHA = os.getenv('DB_SENHA')
+DB_BANCO = os.getenv('DB_BANCO')
+DB_PORTA = os.getenv('DB_PORTA')
 
 def criar_conexao():
-    return mysql.connector.connect(host=host, user=usuario, password=senha, database=banco, port=porta)
+    return mysql.connector.connect(host=DB_HOST, user=DB_USUARIO, password=DB_SENHA, database=DB_BANCO, port=DB_PORTA)
 
 
 def fechar_conexao(con):
